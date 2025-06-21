e07a134bbcfce71a94dc2ce03076ccd690317f2f
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-06-21 04:54:05 -0400

    database connection with mongoose and mongodb atlas

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
-const port = 3000
+
+app.use(
+  cors({
+    origin : process.env.BASE_URL,
+     credentials : true,
+    methods : ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders : ['Content-Type', 'Authorization'] 
+  })
+);
+app.use(express.json()); // to get .json type data from frontend to backend.
+app.use(express.urlencoded({extended:true})) //it helps with your url issues like space between etc.
+
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('Cohort!')
+  res.send('Cohort!');
 })
+
+app.get('/Aditya', (req, res) => {
+  res.send('Aditya!');
+})
+
+app.get('/Anup', (req, res) => {
+  res.send('Anup!');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-console.log(process.env);
---
+++ utils/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+   console.log("connected to MongoDB");
+   
+  })
+  .catch((err) => {
+    console.log("error connecting to MongoDB");
+  })
