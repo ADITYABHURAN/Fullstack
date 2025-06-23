feac73c021736fbb1576da62a3c0829b2912e484
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-06-23 10:17:35 -0400

    defines role

---
+++ model/User.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String // You can add more fields as per your requirements
+    role: {
+        type: String,
+        enum: ['admin', 'user'], // Define roles as per your requirements
+        default: 'user' // Default role is user
+    },
+    isVerified: {
+        type: Boolean,
+        default: false // Default is not verified
+    },
+    VerificationToken: {
+        type: String,
+    },
+    resetPasswordToken: {
+        type: String,
+    },
+    resetPasswordExpires: {
+        type: Date,
+    }
+} {
+    timestamps: true // Automatically manage createdAt and updatedAt fields 
+}) 
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
