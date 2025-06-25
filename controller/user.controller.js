4ca71cc8bef87c8f0e0365254e2b3cae59afaa02
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-06-25 02:57:13 -0400

    all functionality addes

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,7 @@
-import { User } from "../models/user.model.js";
+import { log } from "console";
+import User from "../model/User.model.js";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
 
 const registerUser = async (req, res) => {
 
@@ -11,9 +14,8 @@ const registerUser = async (req, res) => {
     });
   }
 
-
   try {
-   const existinguser = await User.FindOne({ email})
+   const existinguser = await User.findOne({ email })
    if (existinguser) {
     return res.status(400).json({
         message: "User already exists"
@@ -24,17 +26,58 @@ const registerUser = async (req, res) => {
     email,
     password
    }); // create new user
-    
+   console.log(user);
+    // Check if user is created successfully
    if (!user) {
     return res.status(400).json({
         message: "User Not Registered"
     })
    }
+    // Generate a verification token
+   const token = crypto.randomBytes(32).toString("hex");
+   console.log(token);
+    user.verificationToken = token;
+    await user.save();  //await kyuki database dusre continent mai hai . applies to all awaits .
 
 
-  } catch (error) {
-    
-  }
+
+
+    // Send verification email (this part is not implemented in this example)
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_HOST,
+    port: process.env.MAILTRAP_HOST,
+    secure: false, // true for 465, false for other ports
+    auth: {
+    user: process.env.MAILTRAP_USERNAME,
+    pass: process.env.MAILTRAP_PASSWORD,
+  },
+});
+   
+const mailOptions = ({ 
+   from: process.env.MAILTRAP_SENDER_EMAIL,
+    to: user.email, // list of receivers
+    subject: "Verify your email", // Subject line
+    text: `Please click on the following link: 
+     ${process.env.BASE_URL}/api/v1/user/verify/${token}`,
+   
+  });
+
+  await transporter.sendMail(mailOptions)
+  res.status(201).json({
+    message: "User Has been registered Successfully",
+    success: true, 
+  });
+
+
+
+ } catch (error) {
+   res.status(400).json({
+    message: "User Not registered",
+    error,
+    success: false, 
+  });
+
+}
  
   
 };
